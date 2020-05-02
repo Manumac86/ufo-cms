88669f676e0d4b5c3e148c974063c0577d488cea
emmanuel-martinez-olapic <emmanuel.martinez@monotype.com>
2020-05-02 18:51:19 -0300

    fix env

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -12,5 +12,7 @@
 
 // Load environment variables
 // Pending this PR: https://github.com/strapi/strapi/pull/3485
-require('dotenv').config({ path: require('find-config')('.env') });
+if(NODE_ENV !== 'production') {
+  require('dotenv').config({ path: require('find-config')('.env') });
+}
 module.exports = () => { };
